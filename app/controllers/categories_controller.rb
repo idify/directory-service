25aa22b668cb00c790869eed98d31eada623ac49
idify <idify>
2015-06-05 15:35:56 +0530

    changes for vendor owner info and services with description crud

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,31 +19,31 @@ class CategoriesController < ApplicationController
     end
 
     if @category.save
-      redirect_to categories_path
+      redirect_to categories_path, notice: 'Service added sucessfully.'
     else
       render 'new'
     end
   end
 
   def edit
-    @category = Category.find(params[:id])
+    @category = Category.friendly.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
+    @category = Category.friendly.find(params[:id])
 
-    if @category.update_attributes(category_params)
-      redirect_to categories_path
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Service updated sucessfully.'
     else
       render 'edit'
     end
   end
 
   def destroy
-    @category = Category.find(params[:id])
+    @category = Category.friendly.find(params[:id])
 
     if @category.destroy
-      redirect_to categories_path
+      redirect_to categories_path, notice: 'Service deleted.'
     else
       render 'index'
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 
-  validates_format_of :mobile, :presence => true, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
+  #validates_format_of :mobile, :presence => true, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
 
   extend FriendlyId
   friendly_id :category_type, use: [:slugged, :history]
