eec107a858df520bd8c9ef5945c31a3e96a0c5d1
idify <idify>
2015-07-17 15:08:47 +0530

    on vendors page - showing default header and profile pics if not uploaded by vendors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
   def gravatar_for(user, size = 30, title = user.email)
     image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
   end
+
+  def default_header_image
+    image_tag 'header_img_default.png', class: "img-responsive"
+  end
+
+  def default_logo_image
+    image_tag 'profile_pic_default.png'
+  end
+
 end
