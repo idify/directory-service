7b24e6213235a92e7abc0b1372ef7361b2805836
idify <idify>
2015-07-23 16:20:10 +0530

    removed display in popup on facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FACEBOOK_CONFIG[:"#{Rails.env}"]['app_id'],FACEBOOK_CONFIG[:"#{Rails.env}"]['secret'],{
-           :scope => 'email,user_birthday', :display => 'popup', auth_type: 'reauthenticate'}
+           :scope => 'email,user_birthday', auth_type: 'reauthenticate'}
 
   provider :twitter, TWITTER_CONFIG[:"#{Rails.env}"]['api_key'],TWITTER_CONFIG[:"#{Rails.env}"]['api_secret'],
     {
